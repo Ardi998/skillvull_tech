532bcb32befbe988bbd12c6b033f2d88a9a50ac7
Ardi Hermanto <ardi.hermanto95@gmail.com>
2024-02-18 21:30:32 +0700

    Initial commit

--- src/App copy.js
+++ src/App copy.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 function App() {
   const [customerID, setCustomerID] = useState('');
@@ -7,20 +7,8 @@ function App() {
   const [predictedProduct, setPredictedProduct] = useState(null);
   const [error, setError] = useState('');
 
-  useEffect(() => {
-    // Menghapus hasil prediksi sebelumnya saat komponen dimuat
-    localStorage.removeItem('predictedProduct');
-  }, []);
-
   const handlePredict = async () => {
     try {
-      // Pengecekan validitas customer_id
-      if (!customerID.trim()) {
-        setPredictedProduct(null); // Menghapus hasil prediksi
-        setError('Customer ID is required');
-        return;
-      }
-
       const response = await fetch('http://localhost:5000/predict', {
         method: 'POST',
         headers: {
@@ -41,14 +29,7 @@ function App() {
       const data = await response.json();
       setPredictedProduct(data);
       setError('');
-
-      localStorage.setItem('predictedProduct', JSON.stringify({ data, timestamp: new Date().getTime() }));
-
-      setTimeout(() => {
-        window.location.reload();
-      }, 5 * 60 * 1000);
     } catch (error) {
-      setPredictedProduct(null); // Menghapus hasil prediksi
       setError(error.message);
     }
   };
@@ -56,19 +37,19 @@ function App() {
   return (
     <div style={{ textAlign: 'center', fontFamily: 'Arial, sans-serif' }}>
       <h1 style={{ backgroundColor: '#007bff', color: 'white', padding: '20px', borderRadius: '5px', fontSize: '45px' }}>Product Prediction</h1>
-      <div style={{ marginBottom: '10px', padding: '8px' }}>
+      <div style={{ marginBottom: '10px',  padding: '8px' }}>
         <label style={{ marginRight: '20px', fontSize: '25px' }}>Customer ID:</label>
         <input type="text" value={customerID} onChange={(e) => setCustomerID(e.target.value)} style={{ width: '200px', padding: '7px' }} />
       </div>
-      <div style={{ marginBottom: '10px', padding: '8px' }}>
+      <div style={{ marginBottom: '10px',  padding: '8px' }}>
         <label style={{ marginRight: '26px', fontSize: '25px' }}>Page Views:</label>
         <input type="text" value={pageViews} onChange={(e) => setPageViews(e.target.value)} style={{ width: '200px', padding: '7px' }} />
       </div>
-      <div style={{ marginBottom: '10px', padding: '8px' }}>
+      <div style={{ marginBottom: '10px',  padding: '8px' }}>
         <label style={{ marginRight: '28px', fontSize: '25px' }}>Time Spent:</label>
         <input type="text" value={timeSpent} onChange={(e) => setTimeSpent(e.target.value)} style={{ width: '200px', padding: '7px' }} />
       </div>
-      <button onClick={handlePredict} style={{ backgroundColor: '#28a745', color: 'white', padding: '10px', borderRadius: '5px', border: 'none', width: '200px', fontSize: '25px', marginTop: '40px' }}>Predict</button>
+      <button onClick={handlePredict} style={{ backgroundColor: '#28a745', color: 'white', padding: '10px', borderRadius: '5px', border: 'none',  width: '200px', fontSize: '25px', marginTop:'40px' }}>Predict</button>
       {predictedProduct && (
         <div style={{ marginTop: '20px', backgroundColor: '#f8f9fa', padding: '20px', borderRadius: '5px' }}>
           <h2>Predicted Product:</h2>
